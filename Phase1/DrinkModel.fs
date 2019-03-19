6cc0c563c4094fb315a914cf2edfa47837613154
Sara Lopez <slopezalaguero@gmail.com>
2019-03-19 14:38:16 +0100

    Phase 1 code implementation

---
+++ Phase1/DrinkModel.fs
@@ -0,0 +1,41 @@
+﻿module DrinkModel
+
+type DrinkSize = Small = 0 | Medium = 1 | Large = 2
+
+type CupType = 
+    Dark | Americano | Espresso
+    member this.price = 
+        match this with
+        | Dark -> 50m
+        | Americano-> 40m
+        | Espresso  -> 55m
+
+type CanType = 
+    Fanta | Cocacola | Redbull
+    member this.price = 
+        match this with
+        | Fanta -> 25m
+        | Cocacola-> 30m
+        | Redbull  -> 15m
+
+type BottleType = 
+    Water | Milk | Juice
+    member this.price = 
+        match this with
+        | Water -> 5m
+        | Milk-> 7m
+        | Juice  -> 10m
+
+type Cup = { cupType: CupType ; size: DrinkSize }
+type Can = { canType: CanType ; size: DrinkSize }
+type Bottle = { bottleType: BottleType ; size: DrinkSize }
+
+type drinks =
+    | Cup of Cup
+    | Can of Can
+    | Bottle of Bottle
+     member this.Price = 
+        match this with 
+        | Cup (cup) -> cup.cupType.price + (10m * (decimal cup.size))
+        | Can (can) -> can.canType.price + (10m * (decimal can.size))
+        | Bottle (bottle) -> bottle.bottleType.price + (10m * (decimal bottle.size))
---
+++ Phase1/Program.fs
@@ -0,0 +1,33 @@
+﻿open DrinkModel
+
+
+let blackCoffee = Cup( { cupType=Dark; size=DrinkSize.Large } )
+printfn "Large size black coffee cup costs: %M" blackCoffee.Price;;
+
+let americanoCoffee = Cup( { cupType=Americano; size=DrinkSize.Medium } )
+printfn "Medium size americano cup costs: %M" americanoCoffee.Price;;
+
+let espressoCoffee = Cup( { cupType=Espresso; size=DrinkSize.Small } )
+printfn "Small size espresso cup costs: %M" espressoCoffee.Price;;
+
+// Cans
+let fantaCan = Can( { canType=Fanta; size=DrinkSize.Small} )
+printfn "Small size fanta can costs: %M" fantaCan.Price;;
+
+let cocacolaCan = Can( { canType=Fanta; size=DrinkSize.Large} )
+printfn "Large size cocacola can costs: %M" cocacolaCan.Price;;
+
+let redbullCan = Can( { canType=Redbull; size=DrinkSize.Medium} )
+printfn "Medium size redbull can costs: %M" redbullCan.Price;;
+
+// Bottles
+let waterBottle = Bottle( { bottleType=Water; size=DrinkSize.Small} )
+printfn "Small size water bottle costs: %M" fantaCan.Price;;
+
+let milkBottle = Bottle( { bottleType=Milk; size=DrinkSize.Medium} )
+printfn "Medium size milk bottle costs: %M" cocacolaCan.Price;;
+
+let juiceBottle = Bottle( { bottleType=Juice; size=DrinkSize.Large} )
+printfn "Large size juice bottle costs: %M" redbullCan.Price;;
+
+System.Console.ReadKey() |> ignore
