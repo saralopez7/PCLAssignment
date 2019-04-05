3720ccbc1559550feb20ddefa95e426b9eb0c3c3
Sara Lopez <slopezalaguero@gmail.com>
2019-04-05 20:40:30 +0200

    reorganize program.fs

--- Phase1/Program.fs
+++ Phase1/Program.fs
@@ -3,7 +3,37 @@
 [<EntryPoint>]
 let main argv =
     
-    //printfn "Time to make an order...\n"
+    // Create Cups
+    let blackCoffee = Cup(Dark, DrinkSize.Large)
+    printfn "Large size black coffee cup costs: %f" blackCoffee.Price
+    
+    let americanoCoffee = Cup(Americano, DrinkSize.Medium)
+    printfn "Medium size americano cup costs: %f" americanoCoffee.Price
+
+    let espressoCoffee = Cup(Espresso, DrinkSize.Small )
+    printfn "Small size espresso cup costs: %f" espressoCoffee.Price
+
+    // Create Cans
+    let fantaCan = Can(Fanta, DrinkSize.Small)
+    printfn "Small size fanta can costs: %f" fantaCan.Price
+
+    let cocacolaCan = Can(Cocacola, DrinkSize.Large)
+    printfn "Large size cocacola can costs: %f" cocacolaCan.Price
+
+    let redbullCan = Can(Redbull, DrinkSize.Medium)
+    printfn "Medium size redbull can costs: %f" redbullCan.Price
+
+    // Create Bottles
+    let waterBottle = Bottle(Water, DrinkSize.Small)
+    printfn "Small size water bottle costs: %f" waterBottle.Price
+
+    let milkBottle = Bottle(Milk, DrinkSize.Medium)
+    printfn "Medium size milk bottle costs: %f" milkBottle.Price
+
+    let juiceBottle = Bottle(Juice, DrinkSize.Large)
+    printfn "Large size juice bottle costs: %f" juiceBottle.Price
+
+      //printfn "Time to make an order...\n"
     //printfn "Choose Your drink"
     //printfn "1) Dark Coffee"
     //printfn "2) Americano"
@@ -25,35 +55,6 @@ let main argv =
     //let selectedDrink = getSelectedDrink (int userDrinkChoice) (int userDrinkSizeChoice)
     //printfn "A %s %s costs %M dkk" (selectedDrink.Type)  (selectedDrink.Size.ToString()) selectedDrink.Price
 
-    // Cups
-    let blackCoffee = Cup(Dark, DrinkSize.Large)
-    printfn "Large size black coffee cup costs: %f" blackCoffee.Price
-    
-    let americanoCoffee = Cup(Americano, DrinkSize.Medium)
-    printfn "Medium size americano cup costs: %f" americanoCoffee.Price
-
-    let espressoCoffee = Cup(Espresso, DrinkSize.Small )
-    printfn "Small size espresso cup costs: %f" espressoCoffee.Price
-
-    // Cans
-    let fantaCan = Can(Fanta, DrinkSize.Small)
-    printfn "Small size fanta can costs: %f" fantaCan.Price
-
-    let cocacolaCan = Can(Cocacola, DrinkSize.Large)
-    printfn "Large size cocacola can costs: %f" cocacolaCan.Price
-
-    let redbullCan = Can(Redbull, DrinkSize.Medium)
-    printfn "Medium size redbull can costs: %f" redbullCan.Price
-
-    // Bottles
-    let waterBottle = Bottle(Water, DrinkSize.Small)
-    printfn "Small size water bottle costs: %f" waterBottle.Price
-
-    let milkBottle = Bottle(Milk, DrinkSize.Medium)
-    printfn "Medium size milk bottle costs: %f" milkBottle.Price
-
-    let juiceBottle = Bottle(Juice, DrinkSize.Large)
-    printfn "Large size juice bottle costs: %f" juiceBottle.Price
 
     System.Console.ReadKey() |> ignore
     0
